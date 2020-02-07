c1a126c295d1719a5a725aa7598a34f89eeec642
Vishwanath <vishwanathshankar0703@gmail.com>
2020-02-07 15:52:32 +0530

    git ignore

--- src/main/java/football/controllers/FootballController.java
+++ src/main/java/football/controllers/FootballController.java
@@ -2,7 +2,10 @@ package football.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
